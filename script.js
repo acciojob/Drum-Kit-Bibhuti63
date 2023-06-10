5ef84010979de044b65fc2b4abbd5c7f4759b25a
Bibhuti Biswal <91078576+Bibhuti63@users.noreply.github.com>
2023-06-10 13:23:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+window.addEventListener('keydown', function(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+  if (!audio) return;
+
+  audio.currentTime = 1;
+  audio.play();
+
+  key.classList.add('playing');
+});
+
+function removeTransition
